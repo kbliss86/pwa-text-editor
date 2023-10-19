f371ee2f3f3666dfa63f04864f0c088f3cdb842a
kbliss86 <132328278+kbliss86@users.noreply.github.com>
2023-10-19 18:30:12 -0500

    tutor session bug fixes - MVP achieved

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -4,34 +4,30 @@ const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
         return;
       }
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.add({jate: content});
+  const request = store.put({value: content, id: 1});
   const result = await request;
   console.log('Date Saved to Databse', result);
 };
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
   console.log('Date retrieved from Databse', result);
-  return result;
+  return result?.value;
 };
 
 initdb();
